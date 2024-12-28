a2d91747e831abf6ade42ada7c7f4e6a030d9ddc
fgdg <zsolardevofficial@gmail.com>
2024-12-28 13:55:42 -0500

    did chatGPT cook

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.post("/", (req, res) => {
 
   if (rconAuthenticated)
   {
-    conn.send('say Ko-fi dotation!!! message: ' + data.data);
+    conn.send('say Ko-fi dotation!!! message: ' + data.data.message);
   }
 
   // Send a response back to Ko-fi
