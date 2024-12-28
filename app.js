94588bcb9cf09c672cb6423a5ce68e1e6e743fa9
fgdg <zsolardevofficial@gmail.com>
2024-12-28 13:29:00 -0500

    customm

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 const RCON_IP = '192.99.103.218';
 const RCON_PORT = 8130;
 const RCON_PASSWORD = '819VDe1x3l201';
