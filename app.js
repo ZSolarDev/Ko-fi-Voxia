0e04482702ff288ea508cf4b7e0c05ca8319d046
fgdg <zsolardevofficial@gmail.com>
2024-12-28 15:20:01 -0500

    cmon this has to work

--- app.js
+++ app.js
@@ -13,41 +13,46 @@ var rconAuthenticated = false;
 var conn = new rcon(RCON_IP, RCON_PORT, RCON_PASSWORD);
 console.log("loading...");
 conn.on('auth', function() {
-    console.log("RCON Authenticated!!");
-    rconAuthenticated = true;
-  }).on('response', function(str) {
-    console.log("Response: " + str);
-  }).on('error', function(err) {
-    console.log("Error: " + err);
-  }).on('end', function() {
-    console.log("Connection closed");
-    process.exit();
-});
+        console.log("RCON Authenticated!!");
+        rconAuthenticated = true;
+    }).on('response', function(str) {
+        console.log("Response: " + str);
+    }).on('error', function(err) {
+        console.log("Error: " + err);
+    }).on('end', function() {
+        console.log("Connection closed");
+        process.exit();
+    }
+);
 conn.connect();
 
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send("womp womp. Why is bro here? 💀");
+    res.send("womp womp. Why is bro here? 💀");
 });
 
 app.post("/", (req, res) => {
-  var rawBody = req.body;
-  let parsedData;
+    const rawBody = req.body.data;
 
-  if (rconAuthenticated)
-  {
-    parsedData = JSON.parse(rawBody);
-    conn.send('say Ko-fi dotation!!! message: ' + parsedData.data.message);
-    conn.send('say Ko-fi dotation!!! message: ' + parsedData.message);
-    conn.send('say Ko-fi dotation!!! message: ' + parsedData);
-    console.log('say Ko-fi dotation!!! message: ' + parsedData.data.message);
-  }
+    if (rconAuthenticated)
+    {
+        try {
+            const parsedData = JSON.parse(rawBody);
+            conn.send('say Ko-fi dotation!!! message: ' + parsedData.message);
+        } catch (e) {
+            console.error(e);
+            res.status(500).send("Failed to parse JSON data string");
+        }
+    }else{
+        res.status(500).send("RCON not authenticated");
+    }
 
-  // Send a response back to Ko-fi
-  res.status(200).send("Webhook received");
+    // Send a response back to Ko-fi
+    res.status(200).send("OK");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+    console.log(`Server is listening on port ${PORT}`);
 });
