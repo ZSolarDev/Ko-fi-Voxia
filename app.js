ad9c42af494e9d8c9bedd832c1dd5dae6ce01709
fgdg <zsolardevofficial@gmail.com>
2025-03-25 21:23:15 -0400

    we shoould be good..!

--- app.js
+++ app.js
@@ -1,71 +1,33 @@
-var rcon = require('rcon/node-rcon.js');
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = process.env.PORT;
-const RCON_IP = '51.222.147.157';
-const RCON_PORT = 8011;
-const RCON_PASSWORD = '819VDe1x3l201';
-
-var rconAuthenticated = false;
-
-var conn = new rcon(RCON_IP, RCON_PORT, RCON_PASSWORD);
-console.log("loading...");
-conn.on('auth', function() {
-        console.log("RCON Authenticated!!");
-        rconAuthenticated = true;
-    }).on('response', function(str) {
-        console.log("Response: " + str);
-    }).on('error', function(err) {
-        console.log("Error: " + err);
-    }).on('end', function() {
-        console.log("Connection closed");
-        process.exit();
-    }
-);
-conn.connect();
-
-function getRankFromData(data) {
-    for (var i = 0; i < data.shop_items.length; i++) {
-        switch (data.shop_items[i].variation_name)
-        {
-            case 'Squire': return 'donator';
-            case 'Knight': return 'knight';
-            case 'Overlord': return 'overlord';
-            default: break;
-        }
-    }
-    return '';
-}
+const PORT = 3000;
+const VER_TOKEN = process.env.VER_TOKEN;
+var kofiAccounts = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.send("womp womp. Why is bro here? 💀");
+    res.json(kofiAccounts);
 });
 
 app.post("/", (req, res) => {
     const rawBody = req.body.data;
-
-    if (rconAuthenticated)
-    {
-        try {
-            const parsedData = JSON.parse(rawBody);
-            const rank = getRankFromData(parsedData);
-            if (rank != '')
-                conn.send('lp user ' + parsedData.message + ' parent set ' + getRankFromData(parsedData));
-            else
-                res.status(500).send("Failed to get rank from data, a rank might not have been the purchased item.");
-        } catch (e) {
-            console.error(e);
-            res.status(500).send("Failed to parse JSON data string");
+    try {
+        const body = JSON.parse(rawBody);
+        if (body.verification_token == VER_TOKEN && body.type == "Donation") {
+            let account = [body.from_name, body.email];
+            if (!kofiAccounts.includes(account)) {
+                kofiAccounts.push(account);
+                console.log(kofiAccounts);
+            }
         }
-    }else{
-        res.status(500).send("RCON not authenticated");
+    } catch (e) {
+        console.error(e);
+        res.status(500).send("Failed to parse JSON data string");
     }
-
     res.status(200).send("OK");
 });
 
